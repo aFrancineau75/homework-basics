6ca74ee9cbd59bd74cdb32572e6ee9edbe8a4fb8
Alexandre Francineau <francineau.alexandre@gmail.com>
2025-06-01 19:21:36 +0200

    sum, add, prod,quotient

---
+++ Introduction_javascript/script.js
@@ -0,0 +1,32 @@
+function sum (a,b) {
+    a = parseFloat(a)
+    b = parseFloat(b)
+    return a + b;
+};
+
+function diff (a,b) {
+    a = parseFloat(a)
+    b = parseFloat(b)
+    return a - b;
+};
+
+function production (a,b) {
+    a = parseFloat(a)
+    b = parseFloat(b)
+    return a * b;
+};
+
+function quotient (a,b) {
+    a = parseFloat(a)
+    b = parseFloat(b)
+    return a/b;
+}
+
+let num1 = prompt('Enter first number');
+let num2 = prompt('Enter second number');
+
+
+console.log(`${num1} + ${num2} = ${sum(num1,num2)}`);
+console.log(`${num1} - ${num2} = ${diff(num1,num2)}`);
+console.log(`${num1} / ${num2} = ${quotient(num1,num2)}`);
+console.log(`${num1} * ${num2} = ${production(num1,num2)}`);
